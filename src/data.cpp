12be30b72406730099b9d51a8c8183c11119de3b
ryan-way <ryan.keith.way@gmail.com>
2021-12-05 23:03:25 -0800

    removed duplcate test files

--- src/data.cpp
+++ src/data.cpp
@@ -3,6 +3,6 @@
 std::string getDataFile(int day, int part, bool test) {
   std::string file = "data/";
   file += (test? "test" : "solve");
-  file += "/d" + std::to_string(day) + "p" + std::to_string(part) + ".txt";
+  file += "/d" + std::to_string(day) + ".txt";
   return file;
 }
